a718f56b16c0040fe404ee255eb7296fb339bf86
Sylvanus <sylvain.simon@live.fr>
2016-10-09 11:44:08 +0200

    clean

--- src/CoreBundle/Controllers/SidebarMenuController.php
+++ src/CoreBundle/Controllers/SidebarMenuController.php
@@ -6,9 +6,7 @@ class SidebarMenuController{
     
     public function generateAction(){
         
-
         $template = \TwigHelper::render("sidebar_menu.html.twig");
-        
         echo $template;
     }
 }
